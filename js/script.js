f0e3f80385a8874dbfa81b9ed0c39ca792fbb61b
Vu Tran <vutran.270797@gmail.com>
2018-01-25 23:26:45 +0700

    fixed sign-up-page, sign-in-page

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-// $(document).ready(function() {
-//     $("#sign-up").click(function(event) {
-//         event.preventDefault();
-//         $("#sign-up").load("sign-up.html");
-//     });
-// });
-
-// $(document).ready(function() {
-//     $("#sign-in").click(function(event) {
-//         event.preventDefault();
-//         $("#sign-in").load("sign-in.html")
-//     });
-// });
-
-
-// function loadSignIn(event) {
-//     event.preventDefault();
-//     console.log("adsd");
-//     $("#content").load("./sign-in.html");
-// }
-
 function signInPage() {
     document.location.href = "./sign-in.html";
 }
--- js/sign-up.js
+++ js/sign-up.js
@@ -1,6 +1,12 @@
 function signUp() {
     var email_account = $("#email").val();
     var password_account = $("#password").val();
+    var rePassword_account = $("rePassword").val();
+
+    if (rePassword_account != password_account) {
+        alert("Please check your password or email.\nTry again!");
+        document.location.href = "./sign-up.html";
+    }
 
     var request = $.ajax({
         type: "POST",
@@ -14,21 +20,13 @@ function signUp() {
 
     request.done(function(data) {
         console.log(data);
+
     });
 
     request.fail(function(jqXHR, textStatus) {
         console.log("error");
     });
 }
-
-// $(document).ready(function() {
-//     $("#homePage").load("./index.html");
-// });
-
-// $("#cancel").click(function() {
-//     console.log("abcasdfads");
-//     $("#homePage").load("./index.html");
-// });
 
 function cancel(event) {
     document.location.href = "./index.html";
