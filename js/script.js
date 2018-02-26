c54b1e120b4749e4513e13c50bfe255edf24afea
Vu Tran <vutran.270797@gmail.com>
2018-02-26 15:51:35 +0700

    CRUD

--- js/script.js
+++ js/script.js
@@ -61,6 +61,7 @@ $(document).ready(function() {
     $(document).on("click", ".deleteTodoList", function() {
         var deletePrompt = alert("Do you wanna delete title:");
         var id = $(this).attr("list-id");
+        console.log(id);
         $.ajax({
             url: "https://herokutuan.herokuapp.com/task_lists/" + id,
             method: 'DELETE',
@@ -79,24 +80,30 @@ $(document).ready(function() {
     function showTaskTodoList() {
         var listTaskId = $('#inputTaskTodo').attr('list-id');
         $.ajax({
-            url: "https://todo-js-be.herokuapp.com/todo_lists/" + listTaskId + "/todos",
+            url: "https://herokutuan.herokuapp.com/task_lists/" + listTaskId + "/todos",
             method: 'GET',
             headers: { 'access-token': localStorage.getItem('accessToken'), 'uid': localStorage.getItem('uId'), 'client': localStorage.getItem('client') }
         }).done(function(data, textStatus, jqXHR) {
             $('#markDone').html("");
             $('#alreadyDone').html("");
             for (var i = 0; i < data.length; i++) {
-                if (data[i].done == false) {
+                if (data[i].done == null) {
                     $('#markDone').append('<div class="todo-item form-check"><label class="form-check-label"><input type="checkbox" class="form-check-input" todo-id="' + data[i].id + '">' + data[i].name + '</label></div>');
                 } else {
-                    $('#alreadyDone').append('<div class="todo-item form-check"><p><del>' + data[i].name + '</del></p><div class="btn-delete-todo" todo-id="' + data[i].id + '"><i class="far fa-window-close"></i></div></div>');
+                    $('#alreadyDone').append('<div class="todo-item form-check"><p><del>' + data[i].name + '</del></p><div class="btn-delete-todo" todo-id="' + data[i].id + '"><i class="icon-remove"></i></div></div>');
                 }
             }
         });
     }
 
+    $(document).on('click', '.titeTodo', function() {
+        var listTaskId = $(this).attr('list-id');
+        $('#inputTaskTodo').attr('list-id', listTaskId);
+    });
+
     //create task todo list
     $(document).on('keypress', '#inputTaskTodo', function() {
+        //var listTaskId = $(this).attr('list-id');
         if (event.which === 13) {
             var listTaskId = $(this).attr('list-id');
             var taskTodoName = { name: $(this).val() };
@@ -104,7 +111,7 @@ $(document).ready(function() {
             console.log(listTaskId);
             console.log(taskTodoName);
             $.ajax({
-                url: "https://todo-js-be.herokuapp.com/todo_lists/" + listTaskId + "/todos",
+                url: "https://herokutuan.herokuapp.com/task_lists/" + listTaskId + "/todos",
                 method: 'POST',
                 contentType: 'application/json',
                 headers: { 'access-token': localStorage.getItem('accessToken'), 'uid': localStorage.getItem('uId'), 'client': localStorage.getItem('client') },
@@ -114,6 +121,59 @@ $(document).ready(function() {
                 showTaskTodoList();
             });
         }
+    });
+
+    //mark task todo list
+    $(document).on('click', '#markDone .form-check-input', function() {
+        var listTaskId = $('#inputTaskTodo').attr('list-id');
+        var listTodoId = $(this).attr('todo-id');
+        var todoStatus = { done: true };
+        $.ajax({
+            url: "https://herokutuan.herokuapp.com/task_lists/" + listTaskId + "/todos/" + listTodoId,
+            method: 'PATCH',
+            contentType: 'application/json',
+            headers: { 'access-token': localStorage.getItem('accessToken'), 'uid': localStorage.getItem('uId'), 'client': localStorage.getItem('client') },
+            data: JSON.stringify(todoStatus)
+        }).done(function(data, textStatus, jqXHR) {
+            showTaskTodoList();
+        });
+    });
+
+    //mark all task todo list
+    $(document).on('click', '#markDone', function() {
+        var listTaskId = $('#inputTaskTodo').attr('list-id');
+        $.ajax({
+            url: "https://herokutuan.herokuapp.com/task_lists/" + listTaskId + "/todos",
+            method: 'GET',
+            headers: { 'access-token': localStorage.getItem('accessToken'), 'uid': localStorage.getItem('uId'), 'client': localStorage.getItem('client') }
+        }).done(function(data, textStatus, jqXHR) {
+            for (var i = 0; i < data.length; i++) {
+                var listTodoId = data[i].id;
+                var todoStatus = { done: true };
+                $.ajax({
+                    url: "https://herokutuan.herokuapp.com/task_lists/" + listTaskId + "/todos/" + listTodoId,
+                    method: 'PATCH',
+                    contentType: 'application/json',
+                    headers: { 'access-token': localStorage.getItem('accessToken'), 'uid': localStorage.getItem('uId'), 'client': localStorage.getItem('client') },
+                    data: JSON.stringify(todoStatus)
+                }).done(function(data, textStatus, jqXHR) {
+                    showTaskTodoList();
+                });
+            }
+        });
+    });
+
+    //delete task todo list
+    $(document).on('click', '.btn-delete-todo', function() {
+        var listTaskId = $('#inputTaskTodo').attr('list-id');
+        var listTodoId = $(this).attr('todo-id');
+        $.ajax({
+            url: "https://herokutuan.herokuapp.com/task_lists/" + listTaskId + "/todos/" + listTodoId,
+            method: 'DELETE',
+            headers: { 'access-token': localStorage.getItem('accessToken'), 'uid': localStorage.getItem('uId'), 'client': localStorage.getItem('client') }
+        }).done(function(data, textStatus, jqXHR) {
+            showTaskTodoList();
+        });
     });
 
 });
